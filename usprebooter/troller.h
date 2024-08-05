998aba9a7161136f28ae83b939f141ec0ece3552
klutzyT <109645532+klutzyT@users.noreply.github.com>
2024-08-05 12:09:45 -0700

    Update troller.h

--- usprebooter/troller.h
+++ usprebooter/troller.h
@@ -8,6 +8,9 @@
 #include <Foundation/Foundation.h>
 #ifndef troller_h
 #define troller_h
+xpc_connection_t
+xpc_connection_create_mach_service_compat(const char *name, dispatch_queue_t targetq, uint64_t flags)
+asm("_xpc_connection_create_mach_service");
 int userspaceReboot(void);
 int go(bool isBeta, NSString* argument);
 int get_boot_manifest_hash(char hash[97]);
